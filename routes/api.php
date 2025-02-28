27e463ae30b312b70ced5f62654be4c000401633
Hugo De la Rosa <hugo@quopera.com>
2025-02-28 08:35:23 -0800

    feat(auth): login, logout and api auth routes ready

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,21 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    //for testing purposes only
+    Route::get('/protected-route', function () {
+        return response()->json([
+            'message' => 'You are accessing a protected route',
+        ], 200);
+    });
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
